9aeef9c2b3df2a8f7996c738abad1a7d79740dc0
Shivam Singh <shivmsingh02@gmail.com>
2023-07-22 18:26:58 +0530

    Fix Cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-
+const cors = require("cors");
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
