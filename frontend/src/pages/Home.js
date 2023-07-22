91fcb0f7b244bb2d79f160e65cdbbe82a2eeb445
Shivam Singh <shivmsingh02@gmail.com>
2023-07-22 18:49:41 +0530

    Fix Links

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
   useEffect(() => {
     const fetchWorkouts = async () => {
-      const response = await fetch('/api/workouts')
+      const response = await fetch('https://workoutbuddy-32r6.onrender.com/api/workouts')
       const json = await response.json()
 
       if (response.ok) {
